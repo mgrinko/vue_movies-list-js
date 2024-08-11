6fb069280db1b3474ebce5391b9855c39f8cd8e5
Misha Grinko <glukym@gmail.com>
2024-08-11 14:38:40 +0300

    initial code

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<script setup>
+// import movies from './data/movies.json';
+</script>
+
+<template>
+  <!-- eslint-disable max-len -->
+  <div class="page">
+    <div class="page-content">
+      <div class="movies">
+        <div class="card" data-cy="Movie">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                data-cy="MovieImage"
+                src="https://m.media-amazon.com/images/M/MV5BMTY4NjQ5NDc0Nl5BMl5BanBnXkFtZTYwNjk5NDM3._V1_.jpg"
+                alt="Film logo"
+              />
+            </figure>
+          </div>
+
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="./assets/images/imdb-logo.jpeg" alt="imdb" />
+                </figure>
+              </div>
+
+              <div class="media-content">
+                <p class="title is-8" data-cy="MovieTitle">Love Actually</p>
+              </div>
+            </div>
+
+            <div class="content">
+              <p data-cy="MovieDescription">
+                A thief who steals corporate secrets through the use of
+                dream-sharing technology is given the inverse task of planting
+                an idea into the mind of a C.E.O.
+              </p>
+
+              <a
+                href="https://www.imdb.com/title/tt0314331"
+                data-cy="MovieLink"
+              >
+                IMDB
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="card" data-cy="Movie">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                data-cy="MovieImage"
+                src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+                alt="Film logo"
+              />
+            </figure>
+          </div>
+
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-48x48">
+                  <img src="./assets/images/imdb-logo.jpeg" alt="imdb" />
+                </figure>
+              </div>
+
+              <div class="media-content">
+                <p class="title is-8" data-cy="MovieTitle">Inception</p>
+              </div>
+            </div>
+
+            <div class="content">
+              <p data-cy="MovieDescription">
+                Follows the lives of eight very different couples in dealing
+                with their love lives in various loosely interrelated tales all
+                set during a frantic month before Christmas in London, England.
+              </p>
+
+              <a
+                href="https://www.imdb.com/title/tt1375666"
+                data-cy="MovieLink"
+              >
+                IMDB
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sidebar" data-cy="Sidebar">Sidebar will be here</div>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'bulma/css/bulma.css';
+import './assets/main.scss';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
